5c37ff4d0c1830bb9a17b50bbebdc57bdd4b0532
chrisvergaram <chrisvergaram@gmail.com>
2015-08-30 22:45:54 -0500

    Recepcion de parametros

--- CacheSimulator/cacheSimulator.py
+++ CacheSimulator/cacheSimulator.py
@@ -1,3 +1,11 @@
+import sys
+
+
+# Format admitted for the project <workload_file> <policy> <cache size (number of entries)> <extra parameters may go here>
+file = str(sys.argv[1])
+policy = str(sys.argv[2])
+entriesNum = str(sys.argv[3])
+
 
 class cache:
 	def __init__(self, tamano):
