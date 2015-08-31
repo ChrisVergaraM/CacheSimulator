d99c0e52c0669d41c9d79cda39e9d4d3baaf9a74
chrisvergaram <chrisvergaram@gmail.com>
2015-08-31 00:42:38 -0500

    Estructuras para la cache

--- CacheSimulator/cacheSimulator.py
+++ CacheSimulator/cacheSimulator.py
@@ -1,10 +1,15 @@
 import sys
-
-
 # Format admitted for the project <workload_file> <policy> <cache size (number of entries)> <extra parameters may go here>
+
 file = str(sys.argv[1])
 policy = str(sys.argv[2])
 entriesNum = str(sys.argv[3])
+lru = []
+
+def readFile(file):
+	content = open(str(file),'r')
+	contenido = [x.strip('\n') for x in content.readlines()]
+	return contenido
 
 
 class cache:
