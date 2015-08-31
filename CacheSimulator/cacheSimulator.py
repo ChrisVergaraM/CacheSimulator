dd95f1baab8e793b2fb48b4f4b9bba15a1c52329
Angel González <ajgonzal@espol.edu.ec>
2015-08-31 12:05:03 -0500

    Estructura de la Cache y Pruebas

--- CacheSimulator/cacheSimulator.py
+++ CacheSimulator/cacheSimulator.py
@@ -4,14 +4,32 @@ import sys
 file = str(sys.argv[1])
 policy = str(sys.argv[2])
 entriesNum = str(sys.argv[3])
-lru = []
 
-def readFile(file):
-	content = open(str(file),'r')
+def readFile(archivo):
+	content = open(str(archivo),'r')
 	contenido = [x.strip('\n') for x in content.readlines()]
 	return contenido
 
+dataset = readFile(file)
+lru = []
 
 class cache:
-	def __init__(self, tamano):
-		self.size = tamano
+	def __init__(self, algoritm, tamano):
+		self.size = int(tamano)
+		self.method = str(algoritm)
+		
+	def setMethod(self):
+		if(self.method=='LRU'):
+			print 'usare algo'
+		else:
+			print 'no usare nada'
+			
+	def printInfo(self):
+		print 'Evaluando una cache %s con %d entradas.' % (self.method, self.size)
+		print 'Resultados:'
+		print 'Miss rate: x.x% (W misses out of Q references)'
+		print 'Miss rate (warm cache): y.y% (M misses out of Q 50000 references)'
+		print len(dataset)
+
+test = cache(policy,entriesNum)
+test.printInfo()
