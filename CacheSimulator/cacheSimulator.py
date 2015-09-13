33134fe785e30813a61ef7eb908cdfca26b5ca8a
chrisvergaram <chrisvergaram@gmail.com>
2015-09-13 14:11:16 -0500

    Implementacion Algoritmo Clock o de Segunda Oportunidad

--- CacheSimulator/cacheSimulator.py
+++ CacheSimulator/cacheSimulator.py
@@ -4,13 +4,7 @@ import time
 # python cacheSimulator workload.txt LRU 5000
 file = str(sys.argv[1])
 policy = str(sys.argv[2])
-entriesNum = str(sys.argv[3])
-
-def readFile(archivo):
-	content = open(str(archivo),'r')
-	#contenido = []
-	contenido = [x.strip('\n') for x in content.readlines()]
-	return contenido
+entriesNum = str(sys.argv[3]
 
 def cargarFile(archivo):
 	print 'Cargando ' + str(file)
@@ -20,6 +14,10 @@ def cargarFile(archivo):
 	return contenido
 
 dataset = cargarFile(file)
+class clockItem:
+	def __init__(self,content,ind):
+		self.content = content
+		self.ind = str(ind)
 
 class cache:
 	def __init__(self, algoritm, tamano):
@@ -30,12 +28,19 @@ class cache:
 		self.cacheContent =[] #LRU
 		
 	def setMethod(self):
+		temp = dataset
+		clockList = []
+		uniques = []
+		control = []
 		hits = 0
 		miss= 0
 		i = 0
+		cont = 0
 		print 'Evaluando una cache %s con %d entradas.' % (self.method, self.size)
-		inicio = time.time()
+		
+		#LRU
 		if(self.method=='LRU'):
+			inicio = time.time()
 			while i < len(dataset):
 				if(self.data.has_key(dataset[i])):
 					hits = hits+1
@@ -57,10 +62,75 @@ class cache:
 			print 'Hits: ' +str(hits)
 			print 'Algoritmo ejecutado en %s' % str(total)
 			print len(dataset)
+#Implementacion del algoritmo OPTIMO
+# python cacheSimulator workload.txt OPTIMO 5000			
+		if(self.method=='OPTIMO'):
+			inicio = time.time()
+			while i < len(dataset):
+				#Cuando el elemento se repite
+				proximo = temp[i]
+				temp.remove(proximo)
+				if proximo not in temp:
+					#Cuando el elemento nunca se repite en el archivo
+					uniques.append(proximo)
+				else:
+					control.append(temp.index(proximo))
+					control.sort()				
+				if(self.data.has_key(dataset[i])):
+					hits = hits+1										
+				else:
+					miss = miss+1
+					self.missrate = miss
+					if(len(self.data)<self.size):						
+						self.data[dataset[i]] = dataset[i]
+					else:
+						if not control:
+							del self.data[temp[control.pop()]]
+						else:
+							del self.data[uniques.pop()]
+						self.data[dataset[i]] = dataset[i]							
+				i= i+1
+				#print i
+			fin = time.time()
+			total = fin - inicio
+			print len(self.data)
+			print 'Hay %d Misses' % self.missrate
+			print 'Hits: ' +str(hits)
+			print 'Algoritmo ejecutado en %s' % str(total)
+			print len(dataset)
 			
-		else:
-			print 'no usare nada'
-			
+#Implementacion del algoritmo CLOCK
+# python cacheSimulator workload.txt CLOCK 5000			
+		if(self.method=='CLOCK'):
+			puntero = 0
+			inicio = time.time()
+			while i < len(dataset):
+				if(self.data.has_key(dataset[i])):
+					hits = hits+1
+					self.data[dataset[i]] = 1
+					puntero = puntero + 1
+				else:
+					miss = miss+1
+					self.missrate = miss
+					if(len(self.data)<self.size):						
+						self.data[dataset[i]] = 0
+						#element = clockItem(dataset[i],i)
+						#clockList.append(element)
+					else:
+						if not (self.data.get(dataset[puntero])):
+							del self.data[dataset[puntero]]
+						else:
+							while
+							self.data[dataset[puntero]] = 0
+						self.data[dataset[i]] = 0							
+				i= i+1
+			fin = time.time()
+			total = fin - inicio
+			print len(self.data)
+			print 'Hay %d Misses' % self.missrate
+			print 'Hits: ' +str(hits)
+			print 'Algoritmo ejecutado en %s' % str(total)
+			print len(dataset)
 	def printInfo(self):		
 		print 'Resultados:'
 		print 'Miss rate: x.x% (W misses out of Q references)'
